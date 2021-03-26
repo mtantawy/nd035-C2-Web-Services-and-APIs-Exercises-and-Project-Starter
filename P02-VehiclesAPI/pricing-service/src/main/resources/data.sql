e3a8c739dd728b9099dfe572512dc1dac5913e39
Mahmoud Tantawy <me@mtantawy.com>
2021-03-26 15:03:58 +0100

    switch to data rest and h2 database and remove redundant controller and service

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (1, 'USD', 10.1, 1);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (2, 'EUR', 20.2, 2);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (3, 'USD', 30.3, 3);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (4, 'EUR', 40.4, 4);
+INSERT INTO price (id, currency, price, vehicle_id) VALUES (5, 'USD', 50.5, 5);
