b6c92db057ef75dd1294123275064494cd17cc02
Mahmoud Tantawy <me@mtantawy.com>
2021-03-26 15:39:33 +0100

    connect vehicles api service to eureka and use it to obtain uri of maps and pricing services

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -3,13 +3,23 @@ package com.udacity.vehicles;
 import com.udacity.vehicles.domain.manufacturer.Manufacturer;
 import com.udacity.vehicles.domain.manufacturer.ManufacturerRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 /**
  * Launches a Spring Boot application for the Vehicles API,
@@ -18,7 +28,11 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class VehiclesApiApplication {
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public static void main(String[] args) {
         SpringApplication.run(VehiclesApiApplication.class, args);
@@ -47,22 +61,42 @@ public class VehiclesApiApplication {
 
     /**
      * Web Client for the maps (location) API
-     * @param endpoint where to communicate for the maps API
+     * @param serviceId Id of the service in Eureka to look for
      * @return created maps endpoint
      */
     @Bean(name="maps")
-    public WebClient webClientMaps(@Value("${maps.endpoint}") String endpoint) {
-        return WebClient.create(endpoint);
+    public WebClient webClientMaps(@Value("${maps.serviceId}") String serviceId) {
+        return getWebClientForUri(serviceId);
+    }
+
+    private WebClient getWebClientForUri(@Value("${maps.serviceId}") String serviceId) {
+        ServiceInstance serviceInstance =
+                this.discoveryClient.getInstances(serviceId).stream().findFirst().orElseThrow(RuntimeException::new);
+        return WebClient.create(serviceInstance.getUri().toString());
     }
 
     /**
      * Web Client for the pricing API
-     * @param endpoint where to communicate for the pricing API
+     * @param serviceId Id of the service in Eureka to look for
      * @return created pricing endpoint
      */
     @Bean(name="pricing")
-    public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
-        return WebClient.create(endpoint);
+    public WebClient webClientPricing(@Value("${pricing.serviceId}") String serviceId) {
+        return getWebClientForUri(serviceId);
+    }
+
+    @RestController
+    static
+    class ServiceInstanceRestController {
+
+        @Autowired
+        private DiscoveryClient discoveryClient;
+
+        @RequestMapping("/service-instances/{applicationName}")
+        public List<ServiceInstance> serviceInstancesByApplicationName(
+                @PathVariable String applicationName) {
+            return this.discoveryClient.getInstances(applicationName);
+        }
     }
 
 }
